65dccc1820159ce7a329c703b904582569b7e825
tawheeb <tawheebalmahjariu@gmail.com>
2026-04-10 01:31:29 +0300

    إضافة جميع ملفات المشروع

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'features/splash/presentation/splash_view.dart';
+
+void main() {
+  runApp(FruitsMarket());
+}
+
+class FruitsMarket extends StatelessWidget {
+  const FruitsMarket({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const SplashView(),
+    );
+    
+  }
+}
